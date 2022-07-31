12e7704af964758c033301f77d523601ca240db9
BiradarSiddhant02 <pes1202102033@pesu.pes.edu>
2022-07-31 17:06:27 +0530

    links

--- Creatures.py
+++ Creatures.py
@@ -1,7 +1,7 @@
 class Creature:
     def __init__(self, N_wall, S_wall, E_wall, W_wall,
                  NE_corner, NW_corner, SE_corner, SW_corner,
-                 hungry, food_eaten, eggs_laid, node_index, up, down, right, left):
+                 hungry, food_eaten, node_index, up, down, right, left, lay_eggs):
         
         self.N_wall = N_wall
         self.S_wall = S_wall
@@ -15,7 +15,6 @@ class Creature:
         
         self.hungry = hungry
         self.food_eaten = food_eaten
-        self.eggs_laid = eggs_laid
         
         self.up = 0
         self.right = 0
@@ -32,11 +31,37 @@ class Creature:
                            SW_corner : 7,
                            hungry : 8,
                            food_eaten : 9,
-                           eggs_laid : 10,
-                           up : 11,
-                           right : 12,
-                           left : 13,
-                           down : 14}
+                           up : 10,
+                           right : 11,
+                           left : 12,
+                           down : 13,
+                           lay_eggs : 14}
+        
+        # columns - nodes
+        # rows - links 1 : connected 0 : not_connected
+        
+        
+        links = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0],     #N_wall
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0],     #S_wall
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0],     #W_wall
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0],     #E_wall
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0],     #NE_corner
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0],     #NW_corner
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0],     #SE_corner
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0],     #SW_corner
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],     #hungry
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],     #food_eaten
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],     #up
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],     #right
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],     #left
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],     #down
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]     #lay_eggs
+        
+        """
+            mutation zone:
+            links[0 : 9][10 : 14]
+            links[8 : 10][15]
+        """
     
     def get_distance():
         pass
