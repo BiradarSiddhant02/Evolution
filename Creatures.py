0ac84510cc4bccc033e3f72b649f43db354d328e
BiradarSiddhant02 <pes1202102033@pesu.pes.edu>
2022-07-31 14:43:56 +0530

    classes

---
+++ Creatures.py
@@ -0,0 +1,32 @@
+class Creature:
+    def __init__(self, N_wall, S_wall, E_wall, W_wall,
+                 NE_corner, NW_corner, SE_corner, SW_corner,
+                 hungry, food_eaten, eggs_laid):
+        
+        self.N_wall = N_wall
+        self.S_wall = S_wall
+        self.E_wall = E_wall
+        self.W_wall = W_wall
+        
+        self.NE_corner = NE_corner
+        self.NW_corner = NW_corner
+        self.SE_corner = SE_corner
+        self.SW_corner = SW_corner
+        
+        self.hungry = hungry
+        self.food_eaten = food_eaten
+        self.eggs_laid = eggs_laid
+    
+    def get_distance():
+        pass
+        
+    def hunger_level():
+        pass
+    
+    def eat_food():
+        pass
+    
+    def lay_eggs():
+        pass
+    
+    
