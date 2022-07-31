a49e471e6c424b759e323aa293b3289496e48228
BiradarSiddhant02 <pes1202102033@pesu.pes.edu>
2022-07-31 14:56:12 +0530

    nodes

--- Creatures.py
+++ Creatures.py
@@ -1,7 +1,7 @@
 class Creature:
     def __init__(self, N_wall, S_wall, E_wall, W_wall,
                  NE_corner, NW_corner, SE_corner, SW_corner,
-                 hungry, food_eaten, eggs_laid):
+                 hungry, food_eaten, eggs_laid, node_index, up, down, right, left):
         
         self.N_wall = N_wall
         self.S_wall = S_wall
@@ -16,6 +16,27 @@ class Creature:
         self.hungry = hungry
         self.food_eaten = food_eaten
         self.eggs_laid = eggs_laid
+        
+        self.up = 0
+        self.right = 0
+        self.left = 0
+        self.down = 0
+        
+        self.node_index = {N_wall : 0,
+                           S_wall : 1,
+                           E_wall : 2,
+                           W_wall : 3,
+                           NE_corner : 4,
+                           NW_corner : 5,
+                           SE_corner : 6,
+                           SW_corner : 7,
+                           hungry : 8,
+                           food_eaten : 9,
+                           eggs_laid : 10,
+                           up : 11,
+                           right : 12,
+                           left : 13,
+                           down : 14}
     
     def get_distance():
         pass
@@ -28,5 +49,4 @@ class Creature:
     
     def lay_eggs():
         pass
-    
     
