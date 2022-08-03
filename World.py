d472ab9645a15da4013491fbfaf9ef32f2abba86
BiradarSiddhant02 <pes1202102033@pesu.pes.edu>
2022-08-03 17:14:07 +0530

    World modified

--- World.py
+++ World.py
@@ -3,28 +3,27 @@ import random as rd
 
 pygame.init()
 
-Win = pygame.display.set_mode((720, 720))
+Win = pygame.display.set_mode((500, 500))
 pygame.display.set_caption("World")
 
+x = rd.randint(0, 500)
+y = rd.randint(0, 500)
 
-
-width = 6
-height = 6
+width = 10
+height = 10
 v = 2
 
 run = True
-
+while run:
     
-pygame.time.delay(100)
+    pygame.time.delay(100)
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
 
-for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-        run = False
-
-Win.fill((0, 0, 0))
-for i in range(200):
-    x = rd.randint(0, 720)
-    y = rd.randint(0, 720)
+    Win.fill((0, 0, 0))
     pygame.draw.rect(Win, (255, 0, 0), (x, y, width, height))
-pygame.display.update()
+    pygame.display.update()
     
+pygame.quit()
